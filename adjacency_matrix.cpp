c91ec1aa68d4647fe7b1823c5a26b1dba30d49bf
Dmitry Shopyrev <dimashopyrev@mail.ru>
2024-12-02 17:29:02 +0300

    AdjMtrx constructor and write/read

---
+++ adjacency_matrix.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class AdjMatrix
+{
+    int size;
+    int** matrix;
+public:
+    AdjMatrix(int);
+    friend std::ostream& operator<< (std::ostream&, AdjMatrix);
+    void write(std::ifstream&);
+};
+
+AdjMatrix::AdjMatrix(int _size)
+{
+    size = _size;
+    matrix = new int*[size];
+    for (int i = 0; i < size; i++)
+        matrix[i] = new int[size];
+}
+
+
+std::ostream& operator<< (std::ostream& os, AdjMatrix am)
+{
+    for (int i = 0; i < am.size; i++)
+    {
+        for (int j = 0; j < am.size; j++)
+            os << am.matrix[i][j] << " ";
+        os << "\n";
+    }
+    return os;
+}
+
+void AdjMatrix::write(std::ifstream& ifs)
+{
+    std::string str;
+    if (!ifs)
+        return;
+    getline(ifs, str);
+    for (int i = 0; i < size; i++)
+    {
+        getline(ifs, str);
+        for (int j = 0; j < size; j++)
+        {
+            matrix[i][j] = str[j]-'0';
+            str.erase(j, 1);
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include "Pair.cpp"
-
+#include "adjacency_matrix.cpp"
 
 int main()
 {
     std::cout << "Alright!" << std::endl;
     Pair<int, int> p(1, 2);
     std::cout << p << std::endl;
+    AdjMatrix mtr(3);
+    std::ifstream f("adjacency_matrix.txt");
+    mtr.write(f);
+    std::cout << mtr;
+    std::cout << std::endl;
     return 0;
 }
