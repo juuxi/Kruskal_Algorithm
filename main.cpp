08945731dd791b226efbff34297008b3ed704461
Dmitry Shopyrev <dimashopyrev@mail.ru>
2024-12-03 20:47:20 +0300

    counting lines

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <algorithm>
 #include "Timsort.cpp"
 #include "adjacency_matrix.cpp"
 
 int main()
 {
     std::cout << "Alright!" << std::endl;
-    AdjMatrix mtr(3);
     std::ifstream f("adjacency_matrix.txt");
+    int num_of_lines = std::count(std::istreambuf_iterator<char>(f), 
+            std::istreambuf_iterator<char>(), '\n');
+    f.seekg(0);
+    AdjMatrix mtr(num_of_lines);
     mtr.write(f);
     std::cout << mtr;
     std::cout << std::endl;
-    int minrun = count_minrun(3);
+    int minrun = count_minrun(num_of_lines);
     Pair<int, std::string> p1(3, "a b");
     Pair<int, std::string> p2(1, "a c");
     Pair<int, std::string> p3(2, "b c");
